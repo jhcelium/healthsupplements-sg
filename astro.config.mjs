f17780e4af3bcdcdf2a9a7c4d8a1a527fa376755
K.Morita <jhcelium@gmail.com>
2026-06-14 23:16:07 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://healthsupplements.neoi.jp",
   output: "static",
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
 });
